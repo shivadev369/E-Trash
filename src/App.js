f08c189d3ab98a1a4a1e6bd59001e5a1ce93af4a
shivadev369 <shivuji3693@gmail.com>
2024-07-27 14:31:00 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import HeroSection from './components/HeroSection/HeroSection';
-import 'styles.css'
+
 
 const App = () => {
   return (
